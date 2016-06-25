449c725f98109be1dbf9c3b52538371a1db99d57
klowrey <kendall.lowrey@gmail.com>
2016-06-24 20:44:28 -0700

    halfway through revision of darwin model for contacts. simulator changes include printing selected body when clicked. estimator also has weight scaling as an option

--- darwin_hw/robot.h
+++ darwin_hw/robot.h
@@ -5,9 +5,9 @@ class MyRobot {
   public:
     MyRobot() {}
 
-    virtual bool get_sensors(double * time, double* sensor) { }
+    virtual bool get_sensors(double * time, double* sensor) { return false; }
 
-    virtual bool set_controls(double * u, int *pgain, int *dgain) { }
+    virtual bool set_controls(double * u, int *pgain, int *dgain) { return false; }
 
     bool is_running() {return darwin_ok;}
 
